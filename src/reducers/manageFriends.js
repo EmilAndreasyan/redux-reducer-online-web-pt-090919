2050416816cc4bc04bb040508ca703b0d3bca53d
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-07-14 03:27:36 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function manageFriends(state = {
   friends: [],
 }, action) {
@@ -29,3 +30,16 @@ export function manageFriends(state = {
 
   }
 };
+=======
+export function manageFriends(state = {friends: []}, action){
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return ({...state, friends: [...state.friends, action.friend])
+    case "REMOVE_FRIEND":
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+      return ({...state, friends: [...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+      ]})
+  }
+}
+>>>>>>> b62f71c8da633b29313668c41c8185cf9992f0d9
